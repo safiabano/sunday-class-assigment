5350aa1d40ca2844b413ce8486f4053ce47ae901
safiabano <safiabano736@gmail.com>
2023-04-08 13:14:08 +0500

    assignment

---
+++ class-04/assets/js/app.js
@@ -0,0 +1,76 @@
+//===================variable=====================
+// var firstName="safia ";
+// var lastName="bano";
+// console.log(firstName+lastName);
+// var myFirstName="safia";
+// var myLastName="bano";
+// console.log(myFirstName + " "+  myLastName);
+
+//================alert===========================
+//alert("thanks for coming in our website");
+
+// alert("what is your name?");
+//=================var for variable=============
+
+// var num1="22";
+// var num2=5;
+// document.write(num1*num2);
+
+// var apple=10;
+// var mango=20;
+// var orange=(apple+mango);
+// console.log(orange);
+
+//==================bodmas======================
+// var a = 12;
+// var b = 10;
+// var c = 4;
+// var d = a + b * c/  a;
+// console.log(d);
+// let a= 4;   ////
+// let b= 5;
+// let c = 9;
+// let d = a + b * c / b - a;
+// console.log(d);
+
+
+//===========  ++pre increment, post incr++  ,  --pre decr  ,  post decre -- ==============
+
+// var num1=10;
+// var num2=num1++;
+// console.log(num3);
+
+// var num1=10; // 10 +12  -12 + 1+12
+// var op= num1++  +  ++num1  -  num1++  + num1;
+// console.log(op);
+
+//10 +12 -12 +13
+
+// var a=5;  //6    + 7     +7     + 9  + 9 +10
+// var b= ++a  +   ++a  +   a++   +  ++a +  a++  +  a++;
+// console.log(b);
+
+// var a=5;
+// var b= 10;
+// var c= --b  +   ++a   +   a++   -   b--   +   b++  +  ++b  +   a++ ;
+// console.log(c);
+
+
+//==================== start ASSIGNMENT TWO==================
+//=============page 01===========
+// alert ("John Doe");
+// alert ("15 years old");
+// alert ("Certified Mobilr Application Developmet");
+
+//================page 02=======================
+
+ alert("pizza\n pizz \n piz \n pi \n p");
+ alert("My email adress is example@example.com");
+ alert("I am trying to learn from the book A smarter\n way to learn javascript")
+
+ document.write ("Yah!I can write Html content through javascript");
+
+
+ alert("“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”");
+
+ //====================MADE BY SAFIA BANO==================
